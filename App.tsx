78c9369f771127049862e33146a5c2801e273a59
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2024-01-28 20:48:19 -0300

    Criando um sistema de cameras.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,149 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+  Modal,
+  Image,
+} from "react-native";
+import { Camera, CameraType } from "expo-camera";
+import {
+  MutableRefObject,
+  RefObject,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { FontAwesome } from "@expo/vector-icons";
+
+type CameraRef = RefObject<Camera> | null;
 
 export default function App() {
+  const [type, setType] = useState(CameraType.back);
+  const [hasPermission, setHaspermission] = useState<boolean | null>(null);
+  const camRef: CameraRef = useRef(null);
+  const [capturedPhoto, setCapturedPhoto] = useState<string | null>(null);
+  const [open, setOpen] = useState<boolean>(false);
+
+  const takePicture = async () => {
+    if (camRef.current) {
+      const data = await camRef.current.takePictureAsync();
+      setCapturedPhoto(data.uri);
+      setOpen(true);
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHaspermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+
+  if (hasPermission === false) {
+    return (
+      <View>
+        <Text>Acesso Negado</Text>
+      </View>
+    );
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Camera type={type} style={styles.camera} ref={camRef}>
+        <View style={styles.contentButtons}>
+          <TouchableOpacity
+            style={styles.buttonFlip}
+            onPress={() => {
+              setType(
+                type === CameraType.back ? CameraType.front : CameraType.back
+              );
+            }}
+          >
+            <FontAwesome name="exchange" size={23} color="red" />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonCamera} onPress={takePicture}>
+            <FontAwesome name="camera" size={23} color="#fff" />
+          </TouchableOpacity>
+        </View>
+      </Camera>
+      {capturedPhoto && (
+        <Modal animationType="slide" transparent={true} visible={open}>
+          <View style={styles.contentModal}>
+            <TouchableOpacity
+              style={styles.closeButton}
+              onPress={() => setOpen(false)}
+            >
+              <FontAwesome name="close" size={50} color="#fff" />
+            </TouchableOpacity>
+
+            <Image
+              source={{ uri: capturedPhoto }}
+              style={styles.imgPhoto}
+            ></Image>
+          </View>
+        </Modal>
+      )}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+  },
+  camera: {
+    width: "100%",
+    height: "100%",
+  },
+  contentButtons: {
+    flex: 1,
+    backgroundColor: "transparent",
+    flexDirection: "row",
+  },
+  buttonFlip: {
+    position: "absolute",
+    bottom: 50,
+    left: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    margin: 20,
+    height: 50,
+    width: 50,
+    borderRadius: 50,
+  },
+  buttonCamera: {
+    position: "absolute",
+    bottom: 50,
+    right: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "red",
+    margin: 20,
+    height: 50,
+    width: 50,
+    borderRadius: 50,
+  },
+  contentModal: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "flex-end"
+  },
+  imgPhoto: {
+    width: "100%",
+    height: 400,
+    
+  },
+  closeButton: {
+    position: "absolute",
+    top: 10,
+    left: 2,
+    margin: 10
   },
 });
